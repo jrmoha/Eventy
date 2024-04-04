af495a403e911244fcdff21bf45ccdd117153de2
Mustafa <mostafa.dash34@gmail.com>
2024-04-04 06:40:07 +0200

    Search with date

--- src/modules/search/search.helper.ts
+++ src/modules/search/search.helper.ts
@@ -7,6 +7,32 @@ interface IWhere {
     [op: string]: unknown;
   };
 }
+const dateOperators = {
+  today: {
+    [Op.gte]: new Date(),
+    [Op.lt]: new Date(new Date().setDate(new Date().getDate() + 1)),
+  },
+  tomorrow: {
+    [Op.gte]: new Date(new Date().setDate(new Date().getDate() + 1)),
+    [Op.lt]: new Date(new Date().setDate(new Date().getDate() + 2)),
+  },
+  "this-week": {
+    [Op.gte]: new Date(),
+    [Op.lt]: new Date(new Date().setDate(new Date().getDate() + 7)),
+  },
+  "next-week": {
+    [Op.gte]: new Date(new Date().setDate(new Date().getDate() + 7)),
+    [Op.lt]: new Date(new Date().setDate(new Date().getDate() + 14)),
+  },
+  "this-month": {
+    [Op.gte]: new Date(),
+    [Op.lt]: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+  },
+  "next-month": {
+    [Op.gte]: new Date(new Date().setMonth(new Date().getMonth() + 1)),
+    [Op.lt]: new Date(new Date().setMonth(new Date().getMonth() + 2)),
+  },
+};
 class QueryBuilder {
   private readonly query: SearchInput;
   private readonly where: IWhere;
@@ -18,7 +44,7 @@ class QueryBuilder {
   }
 
   build() {
-    const { q, location } = this.query;
+    const { q, location, date } = this.query;
 
     this.where["search"] = {
       [Op.match]: sequelize.fn("websearch_to_tsquery", "english", q),
@@ -27,6 +53,21 @@ class QueryBuilder {
     if (location) {
       this.where["location"] = {
         [Op.iLike]: `%${location}%`,
+      };
+    }
+    if (date) {
+      const key = date as keyof typeof dateOperators;
+      if (!dateOperators[key]) {
+        this.where["date"] = {
+          [Op.gte]: new Date(date),
+        };
+      } else {
+        this.where["date"] = dateOperators[key];
+      }
+    } else if (this.query.from_date && this.query.to_date) {
+      this.where["date"] = {
+        [Op.gte]: new Date(this.query.from_date),
+        [Op.lte]: new Date(this.query.to_date),
       };
     }
 
--- src/modules/search/search.validator.ts
+++ src/modules/search/search.validator.ts
@@ -13,6 +13,19 @@ export const searchSchema = z.object({
       .min(3, "Location must be at least 3 characters long")
       .trim()
       .optional(),
+    date: z
+      .enum([
+        "today",
+        "tomorrow",
+        "this-week",
+        "next-week",
+        "this-month",
+        "next-month",
+      ])
+      .or(z.string().regex(/^\d{4}-\d{2}-\d{2}$/))
+      .optional(),
+      from_date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).optional(),
+      to_date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/).optional(),
   }),
 });
 
